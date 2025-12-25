d9309a5854c31b1613c81c1b66d689f749075b9e
NeverDieOne <altiore@mail.ru>
2025-12-25 12:11:19 +0300

    Add basic implementation for parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+
+#[derive(Debug)]
+struct LogEntry {
+    timestamp: String,
+    log_type: LogLevel,
+    level: String,
+    message: String,
+}
+
+fn parse_log_line(line: &str) -> Option<LogEntry> {
+    let parts = line.splitn(4, ' ').collect::<Vec<&str>>();
+    if parts.len() != 4 {
+        return None;
+    }
+
+    let log_type = match parts[1] {
+        "INFO" => LogLevel::Info,
+        "WARN" => LogLevel::Warning,
+        "ERROR" => LogLevel::Error,
+        _ => return None,
+    };
+
+    return LogEntry {
+        timestamp: parts[0].to_string(),
+        log_type: log_type,
+        level: parts[2].to_string(),
+        message: parts[3].to_string(),
+    }
+    .into();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./src/app.log").expect("Unable to open log file");
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let log_line = match line {
+            Ok(content) => content,
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+                continue;
+            }
+        };
+
+        let log_entry = match parse_log_line(&log_line) {
+            Some(entry) => entry,
+            None => {
+                eprintln!("Failed to parse log line: {}", log_line);
+                continue;
+            }
+        };
+
+        println!("{:?}", log_entry);
+    }
 }
