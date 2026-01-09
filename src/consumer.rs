54c1f7b7f3eb35aa400ecce3799a10ba81c08d86
NeverDieOne <altiore@mail.ru>
2026-01-09 10:55:50 +0300

    Fix text consumer

--- src/consumer.rs
+++ src/consumer.rs
@@ -23,7 +23,7 @@ pub struct TextConsumer;
 impl Consumer for TextConsumer {
     fn consume(&mut self, entry: &LogEntry) -> Result<Vec<Output>, ConsumerError> {
         Ok(vec![Output::Line(format!(
-            "{} [{}] {}: {}",
+            "{} [{}] {}: {}\n",
             entry.timestamp.to_rfc3339(),
             entry.level.as_str(),
             entry.service,
